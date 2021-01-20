516e7dc7c7e6098a7fc6cbb8be250d7cbab07a2d
Greg Hayes <gthelight@gmail.com>
2021-01-19 22:42:38 -0500

    created projects component working as expected

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
+import Projects from "./components/Projects";
+
 import "./App.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>First app in awhile, this will be a portfolio soon!! :D :D :D</p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-      <Footer />
+    <div className="container">
+      <Projects />
     </div>
   );
 }
---
+++ src/data/data.js
@@ -0,0 +1,49 @@
+const projects = [
+  {
+    id: 1,
+    name: "Saltinator",
+    title: "Senior React Developer",
+    duties: [
+      "Worked in remote cross-functional team included Data Scientists, Machine Learning Engineers, and Back End Developers",
+      "Implemented app-wide state management using Redux with Thunk middleware",
+      "Acted as a resource for newer developers and taught Git workflows and best practices",
+      "Deployed with Netlify: https://saltinator.netlify.app",
+      "Front End Repo: https://github.com/best-salitest-hacker-news-trolls/front-end",
+    ],
+  },
+  {
+    id: 2,
+    name: "WorkOut Logs",
+    title: "React Developer",
+    duties: [
+      "Created login/registration page and user dashboard with React",
+      "Utilized GitHub with remote cross-functional team to update and share work",
+      "Deployed with ZEIT: https://front-end-kappa-nine.now.sh",
+      "Front End Repo: https://github.com/weightlifter-lambda/front-end",
+    ],
+  },
+  {
+    id: 3,
+    name: "Event Planner",
+    title: "UI Developer",
+    duties: [
+      "Used HTML, CSS, and LESS compiler to develop marketing and contact pages for app",
+      "Collaborated with remote team to style and populate content for pages",
+      "Learned Git workflow and utilized GitHub to share work with team",
+      "Deployed with Netlify: https://pensive-mirzakhani-6d8023.netlify.app",
+      "UI Repo: https://github.com/build-week-corporate-event-planner-2019/WEB-UI-Greg-Hayes",
+    ],
+  },
+  {
+    id: 4,
+    name: "FoodTruck Trakr",
+    title: "Back End Developer",
+    duties: [
+      "Built Node.js back end with Express for application",
+      "Created database with Knex and SQLite using migrations and seeding",
+      "Wrote unit tests with Supertest and tested endpoints with Postman",
+      "Back End Repo: https://github.com/BW-FoodTruck-TrackR-1/Back-End",
+    ],
+  },
+];
+export default projects;
